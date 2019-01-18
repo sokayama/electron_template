4bb771ac1dc743dbe5bb5f4a624b778319cbf8b9
DESKTOP-4GST3TD\okayama <shuhei-o@digirea.com>
2019-01-18 18:15:55 +0900

    using electron-builder

--- build.js
+++ build.js
@@ -1,33 +1,15 @@
-var packager = require('electron-packager');
-var config = require('./package.json');
+'use strict';
 
-packager({
-    dir: '.',
-    out: './production',
-    name: "appname",
-    platform: 'win32',
-    arch: 'x64',
-    version: '0.36.12',
-    icon: './icon/icon.ico',
-    "app-version" : config.version,
-    overwrite: true,
-    asar: true,
-    prune: true,
-    ignore: "node_modules/(electron-packager|electron-prebuilt|\.bin)|build.js",
-    "version-string" : {
-        CompanyName: "",
-        FileDescription: "appDescription",
-        OriginalFilename: "appFilename",
-        FileVersion: config.version,
-        ProductVersion: config.version,
-        ProductName: "appProductName",
-        InternalName: "appInternalName",
-        LegalCopyright : ""
-    }
-}, function done (err, appPath) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log("build done!");
-	}
+const builder = require('electron-builder');
+const fs = require('fs');
+const packagejson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+
+builder.build({
+    platform: 'win',
+    config: {
+        'appId': `com.example.${packagejson.name}`,
+        'win': {
+            'target': 'zip',
+        },
+    },
 });
